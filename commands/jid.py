b340632328acce0713b38e2f42c273117c0c759a
Corgano Wade <Corgano@live.ca>
2011-06-21 12:13:35 +0800

    Dictionary unicode fix for non unicode systems dictionary.py - now detects and intercepts encodeing errors. Does not effect anything on unicode compatable systems jid.py - added some debugging lines, commented out now

--- commands/jid.py
+++ commands/jid.py
@@ -10,5 +10,10 @@ def process(sender, type, args, client):
 			if args in xmppUtils.rosters[room]:
 				jid = xmppUtils.rosters[room][args] [2]
 				xmppUtils.sendMessage(sender, jid, type)
-
+			#~ else:
+				#~ print "fail at rosters"
+		#~ else:
+			#~ print "fail at room"
+	#~ else:
+		#~ print "fail at args"
 
